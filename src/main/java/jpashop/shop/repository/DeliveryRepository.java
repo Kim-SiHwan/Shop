fe4b5abfa1230bb28bb742622595b8dc09c0eac8
Kim-SiHwan <tjdans4166@naver.com>
2020-09-22 23:08:42 +0900

    Delivery

--- src/main/java/jpashop/shop/repository/DeliveryRepository.java
+++ src/main/java/jpashop/shop/repository/DeliveryRepository.java
@@ -2,6 +2,8 @@ package jpashop.shop.repository;
 
 import jpashop.shop.domain.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeliveryRepository extends JpaRepository<Delivery,Long> {
 }
