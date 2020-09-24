5bbc829df12d55014f2ac67d0e8cda9ce718013c
Kim-SiHwan <tjdans4166@naver.com>
2020-09-24 19:23:59 +0900

    dummy

---
+++ src/main/resources/data.sql
@@ -0,0 +1,38 @@
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('긴 책상', '긴 책상', '1.jpg','책상',50000,10,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('길다란', '길다란 책상', '2.jpg','책상',70000,6,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('2인용 소파', '푹신한 소파', '3.jpg','소파',130000,4,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('쿠셔닝 체어', '푹신한 쿠션의자', '4.jpg','의자',90000,6,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('와이드 킹사이즈 침대', '넓은 공간의 킹사이즈 침대', '5.jpg','침대',250000,5,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('세미 와이드 킹사이즈 침대', '예쁜 색감의 넓은 바디감의 침대', '6.jpg','침대',250000,10,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('세미 와이드 퀸사이즈 침대', '좋은 재질과 바디감을 갖춘 퀸사이즈 침대', '7.jpg','침대',240000,10,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('앤틱 킹사이즈 침대', '디자인이 돋보이는 킹사이즈 침대', '8.jpg','침대',300000,1,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('브라운 와이드 소파', '넓은 바디감의 브라운 컬러 소파', '9.jpg','소파',210000,10,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('블랙 와이드 소파', '넓은 바디감의 블랙 컬러 소파', '10.jpg','소파',210000,4,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('체어 소파', '1인용 인체공학적 디자인의 소파', '11.jpg','소파',120000,10,now());
+
+INSERT INTO product(title,content,url,type,price,quantity,create_date)
+VALUES ('라운드 소파', '실용적인 소파', '12.jpg','소파',130000,8,now());
+
+
+
