8d0c18dfcb03dec4bd1bf2d8f36deac10f169df8
Kim-SiHwan <tjdans4166@naver.com>
2020-09-26 17:04:11 +0900

    Review

--- src/main/java/jpashop/shop/repository/ReviewRepository.java
+++ src/main/java/jpashop/shop/repository/ReviewRepository.java
@@ -1,9 +1,11 @@
 package jpashop.shop.repository;
 
+import jpashop.shop.domain.Product;
 import jpashop.shop.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review,Long> {
+public interface ReviewRepository extends JpaRepository<Review,Long>, QuerydslPredicateExecutor<Review> {
 }
