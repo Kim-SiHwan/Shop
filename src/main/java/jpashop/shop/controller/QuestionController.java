ef390082116d9b02633ac79d1c1bb4306ed06037
Kim-SiHwan <tjdans4166@naver.com>
2020-09-29 18:56:36 +0900

    Q

--- src/main/java/jpashop/shop/controller/QuestionController.java
+++ src/main/java/jpashop/shop/controller/QuestionController.java
@@ -4,6 +4,7 @@ import jpashop.shop.domain.Question;
 import jpashop.shop.dto.pagination.PageMaker;
 import jpashop.shop.dto.pagination.PageVo;
 import jpashop.shop.dto.requestDto.QuestionRequestDto;
+import jpashop.shop.dto.responseDto.QuestionResponseDto;
 import jpashop.shop.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -21,18 +23,24 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question")
+    @GetMapping("/questions")
     public String allQuestions(Model model, PageVo vo){
         PageMaker result = questionService.findAll(vo);
         model.addAttribute("list", result);
         model.addAttribute("vo",vo);
-        return "/shop/allQuestions";
+        return "/shop/questions";
     }
 
-    @PostMapping("/question")
+    @PostMapping("/add_questions")
     public String addQuestion(QuestionRequestDto requestDto,RedirectAttributes rt){
         questionService.addQuestion(requestDto);
         rt.addAttribute("productId",requestDto.getProductId());
         return "redirect:/shop/view";
+    }
+
+    @PutMapping("/update_questions")
+    public String answerForQuestion(QuestionRequestDto requestDto){
+        questionService.answerQuestion(requestDto);
+        return "redirect:/shop/questions";
     }
 }
